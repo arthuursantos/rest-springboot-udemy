ff55f555be9cecd1052c8fc386450a57e3097314
Arthur Santos <arthur.saantos20@gmail.com>
2024-11-04 17:20:49 -0300

    Adiciona tratamento de exceções genérico e customizado

---
+++ rest-springboot-udemy/src/main/java/org/example/restspringbootudemy/controllers/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,41 @@
+package org.example.restspringbootudemy.controllers.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@RestController
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<StandardError> handleAllExceptions(Exception ex, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        StandardError error = StandardError.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return new ResponseEntity<>(error, status);
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public ResponseEntity<StandardError> handleBadRequestExceptions(Exception ex, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = StandardError.builder()
+                .timestamp(Instant.now())
+                .status(status.value())
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return new ResponseEntity<>(error, status);
+    }
+
+}
---
+++ rest-springboot-udemy/src/main/java/org/example/restspringbootudemy/controllers/exceptions/StandardError.java
@@ -0,0 +1,24 @@
+package org.example.restspringbootudemy.controllers.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StandardError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    private Instant timestamp;
+    private Integer status;
+    private String message;
+    private String path;
+}
