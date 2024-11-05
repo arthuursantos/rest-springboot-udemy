fd91283d1772eef22d8e5811ddb9526c442a4b7c
Arthur Santos <arthur.saantos20@gmail.com>
2024-11-05 17:10:52 -0300

    Refatora o código com pacote Services

--- rest-springboot-udemy/src/main/java/org/example/restspringbootudemy/controllers/MathController.java
+++ rest-springboot-udemy/src/main/java/org/example/restspringbootudemy/controllers/MathController.java
@@ -1,6 +1,8 @@
 package org.example.restspringbootudemy.controllers;
 
 import org.example.restspringbootudemy.controllers.exceptions.UnsupportedMathOperationException;
+import org.example.restspringbootudemy.services.MathService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,29 +10,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-    // mapeia uma requisição para um metodo
+    @Autowired
+    private MathService mathService;
+
     @RequestMapping(value = "/sum/{number1}/{number2}")
     public Double sum(
             @PathVariable(value = "number1") String number1,
             @PathVariable(value = "number2") String number2
-    ) throws Exception {
-
-        if (!isNumber(number1) || !isNumber(number2)) {
-            throw new UnsupportedMathOperationException("Set a numeric value.");
-        }
-
-        return convertToDouble(number1)+convertToDouble(number2);
+    ) {
+        return mathService.sum(number1, number2);
     }
 
-    private Double convertToDouble(String strNumber) {
-        String number = strNumber.replaceAll(",", ".");
-        return Double.parseDouble(number);
+    @RequestMapping(value = "/subtraction/{number1}/{number2}")
+    public Double subtraction(
+            @PathVariable(value = "number1") String number1,
+            @PathVariable(value = "number2") String number2
+    ) {
+        return mathService.sub(number1, number2);
     }
 
-    public boolean isNumber(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    @RequestMapping(value = "multiplication/{number1}/{number2}")
+    public Double multiplication(
+            @PathVariable(value = "number1") String number1,
+            @PathVariable(value = "number2") String number2
+    ) {
+        return mathService.mul(number1, number2);
+    }
+
+    @RequestMapping(value = "/division/{number1}/{number2}")
+    public Double division(
+            @PathVariable(value = "number1") String number1,
+            @PathVariable(value = "number2") String number2
+    ) {
+        return mathService.div(number1, number2);
+    }
+
+    @RequestMapping(value = "media/{number1}/{number2}")
+    public Double mean(
+            @PathVariable(value = "number1") String number1,
+            @PathVariable(value = "number2") String number2
+    ) {
+        return mathService.mean(number1, number2);
+    }
+
+    @RequestMapping(value = "squareRoot/{number}")
+    public Double squareRoot(
+            @PathVariable(value = "number") String number
+    ) {
+        return mathService.squareRoot(number);
     }
 
 }
